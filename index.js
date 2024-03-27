347c7aa753971c2e96e9b10da5f7bf59e0d66a59
princessmumbi <princessmumbi001@gmail.com>
2024-03-27 12:40:34 +0300

    objects-lab-complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+const employee = {
+    name:"Sam",
+    streetAddress:"11 Broadway",
+}
+function updateEmployeeWithKeyAndValue (employee,key,value){
+    let employee1 ={...employee}
+    employee1[key] = value
+    return employee1
+}
+function destructivelyUpdateEmployeeWithKeyAndValue (employee,key,value){
+    let employee2 ={...employee}
+    employee2[key] = value
+    employee.streetAddress = "12 Broadway"
+    return employee2
+}
+function deleteFromEmployeeByKey (employee,key){
+    let employee3 ={...employee}
+    delete employee3.name
+    return employee3
+}
+function destructivelyDeleteFromEmployeeByKey (employee){ 
+    delete employee.name
+    return employee
+}
